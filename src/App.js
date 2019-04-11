9a08e03afe80db6c8551e9a5406ae0846f7c6d5f
Steven Tu <stu562@gmail.com>
2019-04-11 12:29:29 -0700

    imported self port, validated working react page

--- src/App.js
+++ src/App.js
@@ -9,15 +9,17 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload or YES NOT.
+            Here's to react for a personal page
           </p>
+          <img src='selfport.jpeg' className="self-port" alt="logo"/>
+
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/stu562/stu562.github.io/tree/development"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            github
           </a>
         </header>
       </div>
