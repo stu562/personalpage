ff3826ce124c0884900b60e92ea6cb3b05c06bd3
Steven Tu <stu562@gmail.com>
2019-04-18 12:52:27 -0700

    working slider, remove bottons

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
     this.state = {
       sideBarOpen: false,
       slideIndex:0,
-      length: 6,
+      length: 3,
       wrapAround: false,
       underlineHeader: false,
       slidesToShow: 1,
@@ -50,7 +50,6 @@ class App extends Component {
     render() {
       let sideBar;
       let backdrop;
-      // let classHide = this.state.isHide ? 'hide' : ''; 
       
       if (this.state.sideBarOpen) {
         sideBar = <SideBar />;
@@ -60,11 +59,14 @@ class App extends Component {
       return (
         <div className="App" style={{height: '100%'}}>
         <Toolbar sideBarClickHandler={this.sideBarToggleClickHandler}/>
+        
+        
 
         {sideBar}
         {backdrop}
         <header className="App-header" >
           <h1>Steven Tu</h1>
+
           <Selfport />
           <Fade top>
             <Aboutme />
@@ -80,33 +82,23 @@ class App extends Component {
               <li>Git</li>
             </ul>
           </Fade>
-            
-         
-          <h1> Portfolio Carousel here as a component </h1>
-          <Portfolio />
+          {/* <iframe width="801" height="451" src='https://www.youtube.com/embed/oVXZTmi2ruI'
+        frameBorder='0'
+        allow='autoplay; encrypted-media'
+        allowFullScreen
+        title='video'
+        /> */}
 
+        <Fade>
           <div style={{ width: "50%", margin: "auto" }}>
       <Carousel
         withoutControls={this.state.withoutControls}
         transitionMode={this.state.transitionMode}
         cellAlign={this.state.cellAlign}
         slidesToShow={this.state.slidesToShow}
-        wrapAround={this.state.wrapAround}
+        wrapAround={!this.state.wrapAround}
         slideIndex={this.state.slideIndex}
         heightMode={this.state.heightMode}
-        renderTopCenterControls={({ currentSlide }) => (
-          <div
-            style={{
-              fontFamily: "Helvetica",
-              color: "#fff",
-              textDecoration: this.state.underlineHeader
-                ? "underline"
-                : "none"
-            }}
-          >
-            Nuka Carousel: Slide {currentSlide + 1}
-          </div>
-        )}
       >
         {colors.slice(0, this.state.length).map((color, index) => (
           <img
@@ -122,49 +114,7 @@ class App extends Component {
           />
         ))}
       </Carousel>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
-          <button onClick={() => this.setState({ slideIndex: 0 })}>1</button>
-          <button onClick={() => this.setState({ slideIndex: 1 })}>2</button>
-          <button onClick={() => this.setState({ slideIndex: 2 })}>3</button>
-          <button onClick={() => this.setState({ slideIndex: 3 })}>4</button>
-          <button onClick={() => this.setState({ slideIndex: 4 })}>5</button>
-          <button onClick={() => this.setState({ slideIndex: 5 })}>6</button>
-        </div>
-        <div>
-          <button
-            onClick={() =>
-              this.setState(prevState => {
-                const length = prevState.length === 6 ? 2 : 6;
-                return {
-                  length
-                };
-              })
-            }
-          >
-            Toggle 2 Slides
-          </button>
-          <button
-            onClick={() =>
-              this.setState({
-                transitionMode:
-                  this.state.transitionMode === "fade" ? "scroll" : "fade"
-              })
-            }
-          >
-            Toggle Fade {this.state.transitionMode === "fade" ? "Off" : "On"}
-          </button>
-          <button
-            onClick={() =>
-              this.setState(prevState => ({
-                wrapAround: !prevState.wrapAround
-              }))
-            }
-          >
-            Toggle Wrap Around
-          </button>
-        </div>
-      </div>
+      
       {this.state.transitionMode !== "fade" && (
         <div style={{ display: "flex", justifyContent: "space-between" }}>
           {this.state.slidesToShow > 1.0 && (
@@ -180,41 +130,13 @@ class App extends Component {
               </button>
             </div>
           )}
-          <div style={{ marginLeft: "auto" }}>
-            <button
-              onClick={() =>
-                this.setState({
-                  slidesToShow: this.state.slidesToShow > 1.0 ? 1.0 : 1.25
-                })
-              }
-            >
-              Toggle Partially Visible Slides
-            </button>
-            <button
-              onClick={() =>
-                this.setState({
-                  heightMode:
-                    this.state.heightMode === "current" ? "max" : "current"
-                })
-              }
-            >
-              Toggle Height Mode Current
-            </button>
-            <button
-              onClick={() =>
-                this.setState({
-                  withoutControls: !this.state.withoutControls
-                })
-              }
-            >
-              Toggle Controls
-            </button>
-          </div>
+
         </div>
-      )}
+      )}  
     </div>
+    </Fade>
           
-          <h1> My tech interest pictured by Instagram </h1>
+          <h1> Coming soon Instagram rendering </h1>
         </header>
       </div>
     );
